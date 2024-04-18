9a8586c7b080b7abe60a45f587ffa3e21f779d5f
nirajwadhe <nirajwadhe@gmail.com>
2024-04-18 15:26:41 +0530

    another new

--- employee_wage.py
+++ employee_wage.py
@@ -4,15 +4,14 @@ def rmdm_attnd(attendance):
     for i in range(1,31):
         attendance[i] = rm.randint(0,1)
 
-n = int(input("Enter a day: "))
-
-def check_attendance(n,attendance):
+def check_attendance(attendance):
+    n = int(input("Enter a day: "))
     if attendance[n]==1:
         print("Employee is Present")
     else :
         print("Employee is asbent")    
 
 rmdm_attnd(attendance)       
-check_attendance(n,attendance)
+check_attendance(attendance)
 
 
