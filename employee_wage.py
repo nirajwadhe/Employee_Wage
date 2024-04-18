c3f102d92156f2972b0396c7291e81e6dac51e8b
nirajwadhe <nirajwadhe@gmail.com>
2024-04-18 16:04:20 +0530

    [Niraj] Add : Added a function of Daily Wages

--- employee_wage.py
+++ employee_wage.py
@@ -9,4 +9,10 @@ def check_attendance():
         
 check_attendance()            
 
+#UC2
+
+def cal_dalily_wage():
+    rate_per_hr = 20
+    hrs_in_day = 8
+    return f"Daily wage of employee is {rate_per_hr*hrs_in_day}"
 
