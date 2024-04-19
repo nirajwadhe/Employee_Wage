a0e2b7b43b801ec2b704830d79618e4c019f893d
nirajwadhe <nirajwadhe@gmail.com>
2024-04-19 10:43:06 +0530

    [Niraj] Refractor : calculated a monthly wage on condition that if employee complete in given condition

--- employee_wage.py
+++ employee_wage.py
@@ -4,38 +4,34 @@ HRS_IN_DAY = 8
 PART_TIME_HRS = 4
 TOTAL_HRS = HRS_IN_DAY + PART_TIME_HRS
 DAYS_IN_MONTH = 20
+
+TOTAL_HRS_WORKED = 0
+TOTAL_DAYS_WORKED = 0
+
 def check_attendance():
-    attendance = rm.randint(0,2)
+        global TOTAL_HRS_WORKED 
+        global TOTAL_DAYS_WORKED
+        while TOTAL_HRS_WORKED<100 or TOTAL_DAYS_WORKED < 20 :
+            attendance = rm.randint(0,2)
+            match attendance :
+                case 1 :
+                    print("Employee is Present")
+                    cal_dalily_wage(RATE_PER_HR, HRS_IN_DAY)
 
-    TOTAL_HRS_WORKED = 0
-    TOTAL_DAYS_WORKED = 0
+                    TOTAL_DAYS_WORKED+=1
+                    TOTAL_HRS_WORKED+=8
 
-    while TOTAL_HRS_WORKED<100 or TOTAL_DAYS_WORKED < 20 :
-        TOTAL_DAYS_WORKED+=1
-        TOTAL_HRS_WORKED+=8
-    match attendance :
-        case 1 :
-            print("Employee is Present")
-            cal_dalily_wage(RATE_PER_HR, HRS_IN_DAY)
 
-            while TOTAL_HRS_WORKED<100 or TOTAL_DAYS_WORKED < 20 :
-                TOTAL_DAYS_WORKED+=1
-                TOTAL_HRS_WORKED+=8
-            cal_monthly_wage(RATE_PER_HR,TOTAL_DAYS_WORKED,TOTAL_HRS_WORKED)
+                case 2:
+                    print("Employee is present and worked parttime")
+                    part_time_wage(RATE_PER_HR, TOTAL_HRS)
 
-        case 2:
-            print("Employee is present and worked parttime")
-            part_time_wage(RATE_PER_HR, TOTAL_HRS)
-
-            while TOTAL_HRS_WORKED<100 or TOTAL_DAYS_WORKED < 20 :
-                TOTAL_DAYS_WORKED+=1
-                TOTAL_HRS_WORKED+=12
-
-            cal_monthly_wage(RATE_PER_HR,TOTAL_HRS_WORKED,TOTAL_DAYS_WORKED)
-        
-        case _:
-            print("Employee is absent")
-                  
+                    TOTAL_DAYS_WORKED+=1
+                    TOTAL_HRS_WORKED+=12
+                
+                case _:
+                    print("Employee is absent")
+                    
 #UC2
 def cal_dalily_wage(wage, hour):
     print("Daily wage of employee is ", wage*hour)
@@ -47,5 +43,7 @@ def part_time_wage(wage, hour):
 #UC5
 def cal_monthly_wage(wage, hour, day):
     print("Monthly Wage of Employee is ", wage*hour*day)
+    
 check_attendance()
+cal_monthly_wage(RATE_PER_HR,TOTAL_DAYS_WORKED,TOTAL_HRS_WORKED)
 
